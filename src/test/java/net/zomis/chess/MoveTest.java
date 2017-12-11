7dad49fe0658465ba62a0ff0623e93b5199cc0ca
Simon Forsberg <zomis2k@hotmail.com>
2017-12-11 18:07:25 +0100

    Split out parts to modules

---
+++ src/test/java/net/zomis/chess/MoveTest.java
@@ -0,0 +1,13 @@
+package net.zomis.chess;
+
+import org.junit.Test;
+
+public class MoveTest {
+
+    @Test
+    public void legalOpeningMoves() {
+        ChessBoard board = ChessBoard.standardGame().moves("d4 e6 e3 Nc6 Nc3");
+        // d4 e6 e3 are pawns (e6 black). c6 is black Knight and c3 is white Knight
+    }
+
+}
